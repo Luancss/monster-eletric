3f9d6025a178cd7f0ac0dc5f1284a9192bc81a9b
Luancss <ventobo123@gmail.com>
2023-12-04 22:30:02 -0300

    Animation canvas html

---
+++ README.md
@@ -0,0 +1,1 @@
+# Monster Eléctrico
---
+++ dist/script.js
@@ -0,0 +1,22 @@
+window.requestAnimFrame = function () {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback);
+    }
+  );
+};
+
+function init(elemid) {
+  let canvas = document.getElementById(elemid),
+    c = canvas.getContext("2d"),
+    w = (canvas.width = window.innerWidth),
+    h = (canvas.height = window.innerHeight);
+  c.fillStyle = "rgba(30,30,30,1)";
+  c.fillRect(0, 0, w, h);
+  return { c: c, canvas: canvas };
+}
