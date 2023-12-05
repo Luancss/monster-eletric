c09f201d4e2370d1b25ce10267166b19f4dcbdb4
Luancss <ventobo123@gmail.com>
2023-12-04 22:41:42 -0300

    add class tentacle

--- dist/script.js
+++ dist/script.js
@@ -71,4 +71,21 @@ window.onload = function () {
       c.lineTo(this.nextPos.x, this.nextPos.y);
     }
   }
+
+  class tentacle {
+    constructor(x, y, l, n, a) {
+      this.x = x;
+      this.y = y;
+      this.l = l;
+      this.n = n;
+      this.t = {};
+      this.rand = Math.random();
+      this.segments = [new segment(this, this.l / this.n, 0, true)];
+      for (let i = 1; i < this.n; i++) {
+        this.segments.push(
+          new segment(this.segments[i - 1], this.l / this.n, 0, false)
+        );
+      }
+    }
+  
     
